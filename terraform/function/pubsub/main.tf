33907078c0814df6cfa8e4e7fd2f2bf49e4b1644
Cas Leentfaar <info@casleentfaar.com>
2021-11-09 16:39:29 +0100

    updated variables

--- terraform/function/pubsub/main.tf
+++ terraform/function/pubsub/main.tf
@@ -5,29 +5,38 @@ resource "google_storage_bucket_object" "functioncode" {
 }
 
 resource "google_cloudfunctions_function" "function" {
-  project = var.project_id
-  # see https://github.com/hashicorp/terraform-provider-google/issues/1938#issuecomment-740532646
-  name = format("%s%s-%s", var.function_name, var.branch_suffix, substr(regex("(?:[a-zA-Z](?:[-_a-zA-Z0-9]{0,30}[a-zA-Z0-9])?)",
-  google_storage_bucket_object.functioncode.md5hash), 0, 10))
-  runtime = "python39"
-
   available_memory_mb   = var.function_memory
+  entry_point           = var.entry_point
+  environment_variables = var.function_env_vars
+  name                  = format("%s%s-%s", var.function_name, var.branch_suffix, substr(regex("(?:[a-zA-Z](?:[-_a-zA-Z0-9]{0,30}[a-zA-Z0-9])?)", google_storage_bucket_object.functioncode.md5hash), 0, 10)) # see https://github.com/hashicorp/terraform-provider-google/issues/1938#issuecomment-740532646
+  project               = var.project_id
+  runtime               = var.runtime
+  service_account_email = google_service_account.cf_sa.email
   source_archive_bucket = google_storage_bucket_object.functioncode.bucket
   source_archive_object = google_storage_bucket_object.functioncode.name
-  entry_point           = "handler"
+  timeout               = var.timeout
 
   event_trigger {
-    event_type = "google.pubsub.topic.publish"
+    event_type = var.event_type
     resource   = var.topic_id
   }
 
-  service_account_email = google_service_account.cf_sa.email
-  environment_variables = var.function_env_vars
-  timeout               = var.timeout
 }
 
 resource "google_service_account" "cf_sa" {
   project      = var.project_id
   account_id   = format("gsa-%s", substr(md5(format("%s%s", var.function_name, var.branch_suffix)), 0, 26))
   display_name = "Service account for ${var.function_name} (branch suffix: ${var.branch_suffix})"
+}
+
+resource "google_project_iam_member" "cf_sa_pubsub" {
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.cf_sa.email}"
+  role    = "roles/pubsub.publisher"
+}
+
+resource "google_project_iam_member" "cf_sa_user" {
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.cf_sa.email}"
+  role    = "roles/iam.serviceAccountUser"
 }
--- terraform/function/pubsub/variables.tf
+++ terraform/function/pubsub/variables.tf
@@ -1,11 +1,19 @@
-variable "bucket_name" {}
 variable "branch_suffix" {}
+variable "bucket_name" {}
+variable "entry_point" {
+  default = "handler"
+}
+variable "event_type" {
+  default = "google.pubsub.topic.publish"
+}
 variable "function_env_vars" {}
 variable "function_memory" {}
 variable "function_name" {}
 variable "path_to_zip_file" {}
 variable "project_id" {}
 variable "region" {}
-variable "source_code_root_path" {}
+variable "runtime" {
+  default = "python39"
+}
 variable "topic_id" {}
 variable "timeout" { default = 60 }
