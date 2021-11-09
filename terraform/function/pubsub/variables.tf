886959810cda01fddfb46ec5c5ee1eb74203e221
Cas Leentfaar <info@casleentfaar.com>
2021-11-09 16:42:52 +0100

    added default memory to reduce required arguments

--- terraform/function/pubsub/variables.tf
+++ terraform/function/pubsub/variables.tf
@@ -7,7 +7,9 @@ variable "event_type" {
   default = "google.pubsub.topic.publish"
 }
 variable "function_env_vars" {}
-variable "function_memory" {}
+variable "function_memory" {
+  default = 128
+}
 variable "function_name" {}
 variable "path_to_zip_file" {}
 variable "project_id" {}
