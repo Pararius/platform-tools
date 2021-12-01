292e28ffc81159d901dbe9c54fc78d382d46345e
Cas <cleentfaar@users.noreply.github.com>
2021-12-01 13:07:17 +0100

    Shortened random value and used it for function naming (#26)
    * shortened random value and used it for function naming
    
    * make sure random value is always compatible with function name

--- terraform/cloud_function/pubsub_triggered/main.tf
+++ terraform/cloud_function/pubsub_triggered/main.tf
@@ -8,7 +8,7 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
-  name                          = format("%s%s", var.function_name, var.branch_suffix)
+  name                          = format("%s%s-%s", var.function_name, var.branch_suffix, random_string.random.result)
   project                       = var.project_id
   runtime                       = var.function_runtime
   service_account_email         = var.function_service_account_email
@@ -25,9 +25,8 @@ resource "google_cloudfunctions_function" "function" {
 }
 
 resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  length           = 4
+  special          = false
 }
 
 resource "google_storage_bucket_object" "functioncode" {
