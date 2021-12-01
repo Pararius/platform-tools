3da9880cfd664fc708535424bed9103ef9ec5a90
Cas Leentfaar <info@casleentfaar.com>
2021-12-01 09:55:15 +0100

    applied feedback

--- terraform/cloud_function/scheduled/main.tf
+++ terraform/cloud_function/scheduled/main.tf
@@ -22,38 +22,38 @@ resource "google_cloudfunctions_function" "function" {
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
   name                          = format("%s%s", var.function_name, var.branch_suffix)
-  project                       = var.project_id
-  region                        = var.project_region
+  project                       = var.function_project_id
+  region                        = var.function_project_region
   runtime                       = var.function_runtime
   service_account_email         = var.function_service_account_email
   source_archive_bucket         = var.source_code_bucket_name
   source_archive_object         = google_storage_bucket_object.functioncode.name
   timeout                       = var.function_timeout
   trigger_http                  = true
-  vpc_connector                 = var.vpc_connector
-  vpc_connector_egress_settings = var.vpc_connector_egress_settings
+  vpc_connector                 = var.function_vpc_connector
+  vpc_connector_egress_settings = var.function_vpc_connector_egress_settings
 }
 
 # Add cloud scheduler job
 resource "google_cloud_scheduler_job" "casco_listing_job" {
-  count    = var.branch_suffix == "" ? 1 : 0 # Only enable job on production to avoid branches eating each others lunch
+  count    = var.scheduler_enabled ? 1 : 0 # Only enable job on production to avoid branches eating each others lunch
   name     = format("%s%s", var.function_name, substr(md5(var.branch_suffix), 0, 26))
-  schedule = var.schedule
+  schedule = var.scheduler_schedule
 
   time_zone        = "Europe/Amsterdam"
-  attempt_deadline = var.attempt_deadline
+  attempt_deadline = var.scheduler_attempt_deadline
 
   retry_config {
-    retry_count = var.retry_count
+    retry_count = var.scheduler_retry_count
   }
 
   http_target {
-    http_method = var.request_method
+    http_method = var.scheduler_request_method
     uri         = google_cloudfunctions_function.function.https_trigger_url
-    body        = base64encode(var.request_body)
+    body        = base64encode(var.scheduler_request_body)
 
     oidc_token {
-      service_account_email = var.iam_invoke_member_email
+      service_account_email = var.scheduler_service_account_email
     }
   }
 }
@@ -65,5 +65,5 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   cloud_function = google_cloudfunctions_function.function.name
 
   role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:${var.iam_invoke_member_email}"
+  member = "serviceAccount:${var.scheduler_service_account_email}"
 }
--- terraform/cloud_function/scheduled/variables.tf
+++ terraform/cloud_function/scheduled/variables.tf
@@ -1,6 +1,3 @@
-variable "attempt_deadline" {
-  default = "320s"
-}
 variable "branch_suffix" {}
 variable "function_entry_point" {
   default = "handler"
@@ -10,31 +7,37 @@ variable "function_env_vars" {
 }
 variable "function_memory" {}
 variable "function_name" {}
+variable "function_project_id" {}
+variable "function_project_region" {}
 variable "function_runtime" {
   default = "python39"
 }
 variable "function_service_account_email" {}
 variable "function_timeout" { default = 60 }
-variable "iam_invoke_member_email" {}
-variable "project_id" {}
-variable "project_region" {}
-variable "request_body" {
-  default = "{}"
+variable "function_vpc_connector" {
+  default = null
 }
-variable "request_method" {
-  default = "POST"
+variable "function_vpc_connector_egress_settings" {
+  default = null
 }
-variable "retry_count" {
+variable "scheduler_attempt_deadline" {
+  default = "320s"
+}
+variable "scheduler_enabled" {
   default = 1
 }
-variable "schedule" {
+variable "scheduler_request_body" {
+  default = "{}"
+}
+variable "scheduler_request_method" {
+  default = "POST"
+}
+variable "scheduler_retry_count" {
+  default = 1
+}
+variable "scheduler_schedule" {
   description = "See https://cloud.google.com/scheduler/docs/configuring/cron-job-schedules"
 }
+variable "scheduler_service_account_email" {}
 variable "source_code_bucket_name" {}
 variable "source_code_root_path" {}
-variable "vpc_connector" {
-  default = null
-}
-variable "vpc_connector_egress_settings" {
-  default = null
-}
