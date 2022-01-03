4a9d96f40c654df33597fa54c41e0f58661449d3
Cas Leentfaar <info@casleentfaar.com>
2022-01-03 18:50:11 +0100

    use managed zip

--- terraform/cloud_function/scheduled/main.tf
+++ terraform/cloud_function/scheduled/main.tf
@@ -4,11 +4,32 @@ Cloud Function invoked by a scheduled job through a HTTP request
 Often used to do things that need to be executed at regular intervals,
 like pulling data from external sources or doing aggregations
 */
+locals {
+  timestamp = formatdate("YYMMDDhhmmss", timestamp())
+  root_dir  = abspath(format("%s%s", var.source_code_root_path, var.function_name))
+}
+
+# Compress source code
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = local.root_dir
+  output_path = "/tmp/function-${local.timestamp}.zip"
+  excludes = [
+    format("%s/tests", local.root_dir),
+    format("%s/.pytest_cache", local.root_dir)
+  ]
+}
+
+resource "google_storage_bucket_object" "functioncode" {
+  name   = format("http_function_sources/%s/sourcecode.zip#%s", var.function_name, data.archive_file.source.output_md5)
+  bucket = var.source_code_bucket_name
+  source = data.archive_file.source.output_path
+}
+
 resource "google_cloudfunctions_function" "function" {
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
-  labels                        = { last_deployed_at = formatdate("YYYYMMDDhhmmss", timestamp()) }
   name                          = format("%s%s", var.function_name, var.branch_suffix)
   project                       = var.project_id
   region                        = var.project_region
@@ -20,12 +41,6 @@ resource "google_cloudfunctions_function" "function" {
   trigger_http                  = true
   vpc_connector                 = var.function_vpc_connector
   vpc_connector_egress_settings = var.function_vpc_connector_egress_settings
-}
-
-resource "google_storage_bucket_object" "functioncode" {
-  name   = format("http_function_sources/%s/sourcecode%s.zip", var.function_name, var.branch_suffix)
-  bucket = var.source_code_bucket_name
-  source = "${var.source_code_root_path}/${var.function_name}/${var.function_name}.zip"
 }
 
 resource "google_cloud_scheduler_job" "scheduler_job" {
