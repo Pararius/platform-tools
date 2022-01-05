936f28b5bb027668a1d39b76878ce54730f20d53
Cas <cleentfaar@users.noreply.github.com>
2022-01-05 15:58:46 +0100

    Revert "Use managed zip (#31)"
    This reverts commit f87ed6f742c9ec2c646eb8fff08c5378ce5604f9.

--- terraform/cloud_function/pubsub_triggered/main.tf
+++ terraform/cloud_function/pubsub_triggered/main.tf
@@ -3,28 +3,11 @@ Cloud Function triggered by a message published on a PubSub topic.
 
 Often used in conjunction with bucket notifications to act on objects created in a bucket.
 */
-locals {
-  timestamp = formatdate("YYMMDDhhmmss", timestamp())
-  root_dir  = abspath(format("%s/%s", var.source_code_root_path, var.function_name))
-}
-
-# Compress source code
-data "archive_file" "source" {
-  type        = "zip"
-  source_dir  = local.root_dir
-  output_path = format("/tmp/pubsub_function_%s.zip", local.timestamp)
-}
-
-resource "google_storage_bucket_object" "functioncode" {
-  name   = format("pubsub_function_sources/%s/sourcecode.zip#%s", var.function_name, data.archive_file.source.output_md5)
-  bucket = var.source_code_bucket_name
-  source = data.archive_file.source.output_path
-}
-
 resource "google_cloudfunctions_function" "function" {
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
+  labels                        = { last_deployed_at = formatdate("YYYYMMDDhhmmss", timestamp()) }
   name                          = format("%s%s", var.function_name, var.branch_suffix)
   project                       = var.project_id
   runtime                       = var.function_runtime
@@ -39,4 +22,10 @@ resource "google_cloudfunctions_function" "function" {
     event_type = "google.pubsub.topic.publish"
     resource   = var.pubsub_topic_id
   }
+}
+
+resource "google_storage_bucket_object" "functioncode" {
+  name   = format("pubsub_function_sources/%s/sourcecode%s.zip", var.function_name, var.branch_suffix)
+  bucket = var.source_code_bucket_name
+  source = format("%s/%s/%s.zip", var.source_code_root_path, var.function_name, var.function_name)
 }
