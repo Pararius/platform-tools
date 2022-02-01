b658ed0f989a71fe3edc1c4f47feaa5a4d01c7d3
M Barendregt <barendregt@fastmail.com>
2022-02-01 14:31:34 +0100

    add support for bucket lifecycle rules

--- terraform/storage/variables.tf
+++ terraform/storage/variables.tf
@@ -1,7 +1,9 @@
 variable "bucket_location" {}
 variable "bucket_name" {}
 variable "enable_versioning" {}
-variable "storage_class" {}
 variable "force_destroy" {
   default = false
 }
+variable "lifecycle_rules" {}
+variable "storage_class" {}
+
