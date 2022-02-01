b1a7b881db7a509be5d11cdb3ff4d2c52258ef44
Martijn Barendregt <88661867+barendregt@users.noreply.github.com>
2022-02-01 16:10:28 +0100

    need a default value for lifecycle ruels (#34)

--- terraform/storage/variables.tf
+++ terraform/storage/variables.tf
@@ -4,6 +4,8 @@ variable "enable_versioning" {}
 variable "force_destroy" {
   default = false
 }
-variable "lifecycle_rules" {}
+variable "lifecycle_rules" {
+  default = []
+}
 variable "storage_class" {}
 
