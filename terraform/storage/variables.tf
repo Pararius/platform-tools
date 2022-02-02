6a52a8ceb96672b434e5298ead479f9c9d8c75ec
M Barendregt <barendregt@fastmail.com>
2022-02-02 10:37:28 +0100

    setup dynamic blocks

--- terraform/storage/variables.tf
+++ terraform/storage/variables.tf
@@ -1,8 +1,23 @@
-variable "bucket_location" {}
-variable "bucket_name" {}
-variable "enable_versioning" {}
+variable "bucket_location" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+variable "enable_versioning" {
+  type    = bool
+  default = true
+}
 variable "force_destroy" {
+  type    = bool
   default = false
 }
-variable "storage_class" {}
+variable "lifecycle_rules" {
+  type    = list(any)
+  default = []
+}
+variable "storage_class" {
+  type    = string
+  default = "STANDARD"
+}
 
