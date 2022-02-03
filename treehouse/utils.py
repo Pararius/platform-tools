74de9ed07fbd1f33cc6270a3528878422246f552
Martijn Barendregt <88661867+barendregt@users.noreply.github.com>
2022-02-03 11:57:51 +0100

    add function to extract param values from prefix (#38)

--- treehouse/utils.py
+++ treehouse/utils.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import re
 
 
 def get_parameters(arg_num: int = 1) -> dict:
@@ -7,3 +8,19 @@ def get_parameters(arg_num: int = 1) -> dict:
     params = json.loads(sys.argv[arg_num])
 
     return params
+
+
+def param_from_prefix(
+    prefix: str, param: str, reg_pattern: str = "([a-zA-Z0-9\.\-\_]+)\/"
+) -> str:
+
+    match = re.search(r"\/" + f"{param}={reg_pattern}", prefix)
+
+    if not match or not match.group(1):
+        raise Exception(
+            f"Failed to extract the value of {param} from the prefix ({prefix})"
+        )
+
+    param_value = match.group(1)
+
+    return param_value
