50104400aba9223c326f06989b3f1d305c46d458
Martijn Barendregt <88661867+barendregt@users.noreply.github.com>
2022-02-03 15:37:35 +0100

    add optional function name prefix (#39)

--- terraform/cloud_function/pubsub_triggered/variables.tf
+++ terraform/cloud_function/pubsub_triggered/variables.tf
@@ -1,26 +1,53 @@
-variable "branch_suffix" {}
+variable "branch_suffix" {
+  type = string
+}
 variable "function_entry_point" {
+  type    = string
   default = "handler"
 }
 variable "function_env_vars" {
+  type    = map(any)
   default = {}
 }
 variable "function_memory" {
+  type    = number
   default = 128
 }
-variable "function_name" {}
+variable "function_name" {
+  type = string
+}
+variable "function_name_prefix" {
+  type    = string
+  default = ""
+}
 variable "function_runtime" {
+  type    = string
   default = "python39"
 }
-variable "function_service_account_email" {}
-variable "function_timeout" { default = 60 }
+variable "function_service_account_email" {
+  type = string
+}
+variable "function_timeout" {
+  type    = number
+  default = 60
+}
 variable "function_vpc_connector" {
+  type    = string
   default = null
 }
 variable "function_vpc_connector_egress_settings" {
+  type    = string
   default = null
 }
-variable "project_id" {}
-variable "pubsub_topic_id" {}
-variable "source_code_bucket_name" {}
-variable "source_code_root_path" {}
+variable "project_id" {
+  type = string
+}
+variable "pubsub_topic_id" {
+  type = string
+}
+variable "source_code_bucket_name" {
+  type = string
+}
+variable "source_code_root_path" {
+  type = stringÎ
+}
