e4be1d2adabf32d5545decbcd4bb48a3f72bb9f2
Cas Leentfaar <info@casleentfaar.com>
2022-02-08 12:20:06 +0100

    trying out file based filtering

--- terraform/cloud_function/scheduled/main.tf
+++ terraform/cloud_function/scheduled/main.tf
@@ -5,24 +5,36 @@ Often used to do things that need to be executed at regular intervals,
 like pulling data from external sources or doing (small) aggregations
 */
 
+locals {
+  excluded_files_list = length(local.include_list) > 0 ? toset([
+    for srcFile in local.source_files :
+    srcFile if contains(local.include_list, srcFile) == false
+  ]) : []
+  include_list = fileexists(format("%s/include.lst", var.source_code_root_path)) ? split(file(format("%s/include.lst", var.source_code_root_path)), "\n") : []
+  source_files = fileset(path.module, format("%s/*.*", var.source_code_root_path))
+}
+
 # Compress source code
 data "archive_file" "source" {
-  type        = "zip"
-  source_dir  = abspath(format("%s/%s", var.source_code_root_path, var.function_name))
+  excludes    = local.excluded_files_list
   output_path = format("/tmp/%s/http_function_%s.zip", var.function_name, formatdate("YYMMDDhhmmss", timestamp()))
+  source_dir  = abspath(format("%s/%s", var.source_code_root_path, var.function_name))
+  type        = "zip"
 }
 
 resource "google_storage_bucket_object" "functioncode" {
-  name   = format("http_function_sources/%s/%s.zip", var.function_name, data.archive_file.source.output_md5)
+  name = format("http_function_sources/%s/%s.zip", var.function_name, data.archive_file.source.output_md5)
+
   bucket = var.source_code_bucket_name
   source = data.archive_file.source.output_path
 }
 
 resource "google_cloudfunctions_function" "function" {
+  name = format("%s%s", var.function_name, var.branch_suffix)
+
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
-  name                          = format("%s%s", var.function_name, var.branch_suffix)
   project                       = var.project_id
   region                        = var.project_region
   runtime                       = var.function_runtime
@@ -64,10 +76,9 @@ resource "google_cloud_scheduler_job" "scheduler_job" {
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
+  cloud_function = google_cloudfunctions_function.function.name
+  member         = "serviceAccount:${var.scheduler_service_account_email}"
   project        = google_cloudfunctions_function.function.project
   region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:${var.scheduler_service_account_email}"
+  role           = "roles/cloudfunctions.invoker"
 }
