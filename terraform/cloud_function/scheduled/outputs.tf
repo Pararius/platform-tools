5c0b1b05790ff9f2425c34c55a9b8d684edfbfb6
Cas Leentfaar <info@casleentfaar.com>
2022-02-08 20:27:10 +0100

    moved to outputs

--- terraform/cloud_function/scheduled/outputs.tf
+++ terraform/cloud_function/scheduled/outputs.tf
@@ -1,3 +1,11 @@
 output "https_trigger_url" {
   value = google_cloudfunctions_function.function.https_trigger_url
 }
+
+output "excluded_files" {
+  value = join(",", local.excluded_files)
+}
+
+output "include_list" {
+  value = join(",", local.include_list)
+}
