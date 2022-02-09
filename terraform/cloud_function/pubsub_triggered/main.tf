23b4f6e067fddadf12c919d595bc758130e1e3ce
Cas <cleentfaar@users.noreply.github.com>
2022-02-09 14:58:17 +0100

    Retry of selfzipping solution (#41)
    * retry of 3rd attempt
    
    * also moved timestamp for consistency adjusted doc
    
    * randomize output path
    
    * trying out file based filtering
    
    * debugging excludes
    
    * fixed format and cs
    
    * fixed source files
    
    * added outputs
    
    * fixed join
    
    * temp disabled excludes
    
    * temp disabled excludes
    
    * moved to outputs
    
    * fixed fileset and splitting
    
    * seems to work lets try
    
    * made consistent and cleaned up debugging

--- terraform/cloud_function/pubsub_triggered/main.tf
+++ terraform/cloud_function/pubsub_triggered/main.tf
@@ -1,14 +1,40 @@
 /*
 Cloud Function triggered by a message published on a PubSub topic.
 
-Often used in conjunction with bucket notifications to act on objects created in a bucket.
+Often used in conjunction with bucket notifications to act on newly created objects.
 */
+
+locals {
+  excluded_files = length(local.include_list) > 0 ? toset([
+    for srcFile in local.source_files :
+    srcFile if contains(local.include_list, srcFile) == false
+  ]) : []
+  include_list  = fileexists(format("%s/include.lst", local.source_prefix)) ? split("\n", file(format("%s/include.lst", local.source_prefix))) : []
+  source_files  = fileset(local.source_prefix, "**")
+  source_prefix = format("%s/%s", var.source_code_root_path, var.function_name)
+}
+
+# Compress source code
+data "archive_file" "source" {
+  excludes    = local.excluded_files
+  output_path = format("/tmp/%s/pubsub_function_%s.zip", var.function_name, formatdate("YYMMDDhhmmss", timestamp()))
+  source_dir  = abspath(format("%s/%s", var.source_code_root_path, var.function_name))
+  type        = "zip"
+}
+
+resource "google_storage_bucket_object" "functioncode" {
+  name = format("pubsub_function_sources/%s/%s.zip", var.function_name, data.archive_file.source.output_md5)
+
+  bucket = var.source_code_bucket_name
+  source = data.archive_file.source.output_path
+}
+
 resource "google_cloudfunctions_function" "function" {
+  name = format("%s%s", var.function_name, var.branch_suffix)
+
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
-  labels                        = { last_deployed_at = formatdate("YYYYMMDDhhmmss", timestamp()) }
-  name                          = format("%s%s%s", var.function_name_prefix, var.function_name, var.branch_suffix)
   project                       = var.project_id
   runtime                       = var.function_runtime
   service_account_email         = var.function_service_account_email
@@ -22,10 +48,4 @@ resource "google_cloudfunctions_function" "function" {
     event_type = "google.pubsub.topic.publish"
     resource   = var.pubsub_topic_id
   }
-}
-
-resource "google_storage_bucket_object" "functioncode" {
-  name   = format("pubsub_function_sources/%s%s/sourcecode%s.zip", var.function_name_prefix, var.function_name, var.branch_suffix)
-  bucket = var.source_code_bucket_name
-  source = format("%s/%s/%s.zip", var.source_code_root_path, var.function_name, var.function_name)
 }
--- terraform/cloud_function/scheduled/main.tf
+++ terraform/cloud_function/scheduled/main.tf
@@ -2,30 +2,50 @@
 Cloud Function invoked by a scheduled job through a HTTP request
 
 Often used to do things that need to be executed at regular intervals,
-like pulling data from external sources or doing aggregations
+like pulling data from external sources or doing (small) aggregations
 */
+
+locals {
+  excluded_files = length(local.include_list) > 0 ? toset([
+    for srcFile in local.source_files :
+    srcFile if contains(local.include_list, srcFile) == false
+  ]) : []
+  include_list  = fileexists(format("%s/include.lst", local.source_prefix)) ? split("\n", file(format("%s/include.lst", local.source_prefix))) : []
+  source_files  = fileset(local.source_prefix, "**")
+  source_prefix = format("%s/%s", var.source_code_root_path, var.function_name)
+}
+
+# Compress source code
+data "archive_file" "source" {
+  excludes    = local.excluded_files
+  output_path = format("/tmp/%s/http_function_%s.zip", var.function_name, formatdate("YYMMDDhhmmss", timestamp()))
+  source_dir  = abspath(format("%s/%s", var.source_code_root_path, var.function_name))
+  type        = "zip"
+}
+
+resource "google_storage_bucket_object" "functioncode" {
+  name = format("http_function_sources/%s/%s.zip", var.function_name, data.archive_file.source.output_md5)
+
+  bucket = var.source_code_bucket_name
+  source = data.archive_file.source.output_path
+}
+
 resource "google_cloudfunctions_function" "function" {
+  name = format("%s%s", var.function_name, var.branch_suffix)
+
   available_memory_mb           = var.function_memory
   entry_point                   = var.function_entry_point
   environment_variables         = var.function_env_vars
-  labels                        = { last_deployed_at = formatdate("YYYYMMDDhhmmss", timestamp()) }
-  name                          = format("%s%s", var.function_name, var.branch_suffix)
   project                       = var.project_id
   region                        = var.project_region
   runtime                       = var.function_runtime
   service_account_email         = var.function_service_account_email
-  source_archive_bucket         = var.source_code_bucket_name
+  source_archive_bucket         = google_storage_bucket_object.functioncode.bucket
   source_archive_object         = google_storage_bucket_object.functioncode.name
   timeout                       = var.function_timeout
   trigger_http                  = true
   vpc_connector                 = var.function_vpc_connector
   vpc_connector_egress_settings = var.function_vpc_connector_egress_settings
-}
-
-resource "google_storage_bucket_object" "functioncode" {
-  name   = format("http_function_sources/%s/sourcecode%s.zip", var.function_name, var.branch_suffix)
-  bucket = var.source_code_bucket_name
-  source = "${var.source_code_root_path}/${var.function_name}/${var.function_name}.zip"
 }
 
 resource "google_cloud_scheduler_job" "scheduler_job" {
@@ -57,10 +77,9 @@ resource "google_cloud_scheduler_job" "scheduler_job" {
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
+  cloud_function = google_cloudfunctions_function.function.name
+  member         = "serviceAccount:${var.scheduler_service_account_email}"
   project        = google_cloudfunctions_function.function.project
   region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "serviceAccount:${var.scheduler_service_account_email}"
+  role           = "roles/cloudfunctions.invoker"
 }
