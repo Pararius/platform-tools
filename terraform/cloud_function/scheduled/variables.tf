40bfd01652e627a8a65878ba2a1f7b5c0217d847
Martijn Barendregt <88661867+barendregt@users.noreply.github.com>
2022-02-10 13:18:16 +0100

    add optional prefix to function names (#42)

--- terraform/cloud_function/scheduled/variables.tf
+++ terraform/cloud_function/scheduled/variables.tf
@@ -3,31 +3,56 @@ terraform {
   experiments = [module_variable_optional_attrs]
 }
 
-variable "branch_suffix" {}
+variable "branch_suffix" {
+  type = string
+}
 variable "function_entry_point" {
+  type    = string
   default = "handler"
 }
 variable "function_env_vars" {
+  type    = map(any)
   default = {}
 }
 variable "function_memory" {
+  type    = number
   default = 128
 }
-variable "function_name" {}
+variable "function_name" {
+  type = string
+}
+variable "function_name_prefix" {
+  type    = string
+  default = ""
+}
 variable "function_runtime" {
+  type    = string
   default = "python39"
 }
-variable "function_service_account_email" {}
-variable "function_timeout" { default = 60 }
+variable "function_service_account_email" {
+  type = string
+}
+variable "function_timeout" {
+  type    = number
+  default = 60
+}
 variable "function_vpc_connector" {
+  type    = string
   default = null
 }
 variable "function_vpc_connector_egress_settings" {
+  type    = string
   default = null
 }
-variable "project_id" {}
-variable "project_region" {}
-variable "scheduler_service_account_email" {}
+variable "project_id" {
+  type = string
+}
+variable "project_region" {
+  type = string
+}
+variable "scheduler_service_account_email" {
+  type = string
+}
 variable "schedulers" {
   default = []
   type = list(object({
@@ -39,5 +64,9 @@ variable "schedulers" {
     retry_count      = optional(number)
   }))
 }
-variable "source_code_bucket_name" {}
-variable "source_code_root_path" {}
+variable "source_code_bucket_name" {
+  type = string
+}
+variable "source_code_root_path" {
+  type = string
+}
