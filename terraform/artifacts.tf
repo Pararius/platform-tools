4876dd22282192f62d2a2a2e241b780c8f6ba5d2
M Barendregt <barendregt@fastmail.com>
2022-08-09 15:17:31 +0200

    remove dash

--- terraform/artifacts.tf
+++ terraform/artifacts.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket_object" "platform-tools-artifact" {
-  name = format("platform_tools/%s/%s", local.branch_suffix == "" ? "latest" : local.branch_suffix, var.platform_tools_artifact_filename)
+  name = format("platform_tools/%s/%s", local.branch_suffix == "" ? "latest" : var.git_branch, var.platform_tools_artifact_filename)
 
   bucket = module.platform-artifacts-bucket.bucket_name
   source = format("../%s", var.platform_tools_artifact_filename)
