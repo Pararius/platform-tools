122531aeccf02d36a1c5164b50c0f0ba2ac90528
cleentfaar <info@casleentfaar.com>
2023-06-28 10:06:21 +0200

    hardcoded dataset (no data block possible)

--- terraform/routines.tf
+++ terraform/routines.tf
@@ -1,16 +1,11 @@
-data "google_bigquery_dataset" "fn" {
-  dataset_id  = "fn"
-  description = "Collection of standard and custom routines used across the platform"
-  project     = local.google_project_id
-  location    = local.region
-}
-
 resource "google_bigquery_routine" "greatest_non_null" {
-  dataset_id      = data.google_bigquery_dataset.fn.dataset_id
+  dataset_id      = "fn" # manually created dataset so have to hardcode (there is no data block for datasets yet, see: https://github.com/hashicorp/terraform-provider-google/issues/5693)
+  definition_body = "(SELECT MAX(y) FROM UNNEST(x) AS y)"
+  language        = "SQL"
+  project         = local.google_project_id
   routine_id      = "greatest_non_null"
   routine_type    = "SCALAR_FUNCTION"
-  language        = "SQL"
-  definition_body = "(SELECT MAX(y) FROM UNNEST(x) AS y)"
+
   arguments {
     name          = "x"
     argument_kind = "ANY_TYPE"
