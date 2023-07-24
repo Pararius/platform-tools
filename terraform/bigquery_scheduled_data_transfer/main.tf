4e97f8eb6e919ed93e7480459070040ac2b4927e
M. Barendregt <barendregt@fastmail.com>
2023-07-24 15:16:08 +0200

    add BQ data transfer module

---
+++ terraform/bigquery_scheduled_data_transfer/main.tf
@@ -0,0 +1,38 @@
+locals {
+  bigquery_cronjob_mappings = {
+    "hourly" : "every hour",
+    "daily" : "every day 00:00",
+    "weekly" : "every monday 00:00",
+    "quarterly" : "1 of jan,april,july,oct 00:00"
+  }
+  bigquery_interval_mappings = {
+    "hourly" : "1 HOUR",
+    "daily" : "1 DAY",
+    "weekly" : "1 WEEK",
+    "quarterly" : "1 QUARTER"
+  }
+}
+
+resource "google_bigquery_data_transfer_config" "default" {
+  data_source_id = "scheduled_query"
+  display_name   = var.name
+  location       = var.location
+
+  destination_dataset_id = var.destination_dataset_id
+
+  params = {
+    query = templatefile(
+      var.query_template,
+      merge(var.query_variables, { interval = try(local.bigquery_interval_mappings[var.interval], var.interval) })
+    )
+    destination_table_name_template = var.destination_table_name_template
+    write_disposition               = var.write_disposition
+  }
+
+  # formatting rules are quite hard to generalize, see: https://cloud.google.com/appengine/docs/flexible/scheduling-jobs-with-cron-yaml#cron_yaml_The_schedule_format
+  schedule = try(local.bigquery_cronjob_mappings[var.interval], var.interval)
+
+  schedule_options {
+    disable_auto_scheduling = var.enable_auto_scheduling ? false : true
+  }
+}
---
+++ terraform/bigquery_scheduled_data_transfer/variables.tf
@@ -0,0 +1,57 @@
+variable "name" {
+  type        = string
+  description = "A unique (display) name describing this data transfer"
+}
+
+variable "interval" {
+  type        = string
+  description = "the interval at which this query should be executed, also affects `interval` variable available inside the query"
+}
+
+variable "enable_auto_scheduling" {
+  type        = bool
+  default     = false
+  description = "Whether this data transfer should be scheduled as soon as its deployed (production), or only triggered by hand (development)."
+}
+
+variable "location" {
+  type        = string
+  description = "Google Cloud location used to execute the data transfer. To reduce costs it is recommended to keep this the same as the tables where data is transferred from/to."
+}
+
+variable "query_template" {
+  type        = string
+  description = "The path to query to execute. Your SQL can make use of the `interval` variable to slice data to the proper time period."
+}
+
+variable "query_variables" {
+  type        = map(string)
+  default     = {}
+  description = "Optional variables to pass to your query template."
+}
+
+variable "destination_dataset_id" {
+  type        = string
+  default     = null
+  description = "The destination dataset to load the data into (if any)."
+}
+
+variable "destination_table_name_template" {
+  type        = string
+  default     = null
+  description = "The destination table to load the data into (if any)."
+}
+
+variable "write_disposition" {
+  type        = string
+  default     = null
+  description = <<EOF
+Specifies the action that occurs if the destination table already exists. The following values are supported:
+
+    WRITE_TRUNCATE: If the table already exists, BigQuery overwrites the table data and uses the schema from the query result.
+    WRITE_APPEND: If the table already exists, BigQuery appends the data to the table.
+    WRITE_EMPTY: If the table already exists and contains data, a 'duplicate' error is returned in the job result.
+
+The default value is WRITE_EMPTY. Each action is atomic and only occurs if BigQuery is able to complete the job successfully. Creation, truncation and append actions occur as one atomic update upon job completion."
+EOF
+}
